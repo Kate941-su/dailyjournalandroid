e4ed9ac83560bc45c5cba051505741caf3360bb6
kitaya <kkitaya@nzigen.com>
2024-04-22 07:20:08 +0900

    add: first commit

---
+++ dailyjournalios/dailyjournaliosApp.swift
@@ -0,0 +1,17 @@
+//
+//  dailyjournaliosApp.swift
+//  dailyjournalios
+//
+//  Created by KaitoKitaya on 2024/04/22.
+//
+
+import SwiftUI
+
+@main
+struct dailyjournaliosApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
